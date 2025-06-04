64ed5ac66a0872e7d2040550d6d3809d8d1c3b77
chalilnandu <nm9346@srmist.edu.in>
2025-06-04 20:26:09 +0530

    new commit

---
+++ src/robot_controller_sept03/robot_controller_sept03/myfirstnode.py
@@ -0,0 +1,18 @@
+
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+    def __init__(self,varrl):
+        super().__init__("first_node")
+        self.get_logger().info("Hello from ROS 2")
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode('Batch2')
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__=='__main__':
+    main()
--- src/robot_controller_sept03/setup.py
+++ src/robot_controller_sept03/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "test_node=robot_controller_sept03.myfirstnode:main"
         ],
     },
 )
