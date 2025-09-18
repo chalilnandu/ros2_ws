3fc36ea7a4a73bfabd5b72c47446bf96f724ff95
chalilnandu <nm9346@srmist.edu.in>
2025-09-18 09:55:51 +0530

    sept18

--- src/robot_controller_sept03/robot_controller_sept03/myfirstnode.py
+++ src/robot_controller_sept03/robot_controller_sept03/myfirstnode.py
@@ -3,14 +3,16 @@ import rclpy
 from rclpy.node import Node
 
 class MyNode(Node):
-    def __init__(self,varrl):
+    def __init__(self):
         super().__init__("first_node")
-        self.get_logger().info("Hello from ROS 2")
+        self.create_timer(1.0,self.timer_callback)
+    def timer_callback(self):
+        self.get_logger().info("ROS2")
         
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MyNode('Batch2')
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
